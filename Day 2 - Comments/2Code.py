bc52fb30a299a0734d5c5a05619b7ff8b827a8f6
Daliah Aljutayli <DaliahAljutayli@gmail.com>
2019-08-22 00:20:17 +0300

    Comments

---
+++ Day 2 - Comments/2Code.py
@@ -0,0 +1,15 @@
+#By Daliah Aljutayli
+
+"""
+Hello,
+This is the 2nd day of 100DaysOfCodeing
+I will Inshaallah work hard, So watch me good luck 
+"""
+
+
+print("Hello, from Other World!") #Print Statement
+
+
+if 10 > 5:
+    print(10 , "Is greater than ",5)
+
