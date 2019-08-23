105c36ffd8b46e7e07b6da4004139d16656d7d2b
Daliah Aljutayli <DaliahAljutayli@gmail.com>
2019-08-23 16:03:59 +0300

    Numbers
    Types of Numbers in Python
    Print The Types of Numbers
    Type Conversion

---
+++ ‏‏Day 4 - PythonNumbers/PythonNumbers.py
@@ -0,0 +1,28 @@
+#Numbers
+
+#1 Types of Numbers in Python
+x = 5 #int
+y = 11.5 #float
+f = 3E3 #Power
+z = 3j #Complex
+print('Integer: ',x,' Float: ',y,' Power: ',f,' Complex: ',z)
+
+#2 Print The Types of Numbers
+x = 5 
+y = 11.5 
+z = 3j 
+print( type(x),type(y),type(z) )
+
+#3 Type Conversion
+x = 5 #int
+print(x,' Convert into float: ',float(x),', into Complex: ',complex(x))
+
+y = 11.5 #float
+print(y,' Convert into int: ',int(y),', into Complex: ',complex(y))
+
+
+
+
+
+
+
