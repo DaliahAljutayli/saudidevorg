d466dd1e40a7a728b97cd89b4c0b16e954c975ba
Daliah Aljutayli <DaliahAljutayli@gmail.com>
2019-08-23 15:14:33 +0300

    Variables

---
+++ ‏‏Day 3 - Variables/Variables.py
@@ -0,0 +1,25 @@
+#Variables 
+
+#1 How to Creating Variables
+x = 5  #number
+y = 'Hello' #string
+print ('Resualt of Creating Variables: ',x,y)
+
+#2 Assign Value to Multiple Variables
+x , y , z = "1st Value " , "2nd Value " ,"3rd Value" 
+print ('Resualt of Assign Value to Multiple Variables: ',x,y,z)
+
+#3 Output Variables: add string with number
+x = 'Daliah '
+y = '2019'
+print ('Resualt of Output Variables: string + number: ',x,y) 
+
+#4 Output Variables: string + string
+x = 'Daliah'
+y = 'Aljutayli'
+print ('Resualt of Output Variables: string + string: ',x,y)
+
+#5 Output Variables: number + number
+x = 6
+y = 4
+print ('Resualt of Output Variables: number + number: ',x,y)
