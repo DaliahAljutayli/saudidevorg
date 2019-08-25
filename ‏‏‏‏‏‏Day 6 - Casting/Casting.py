f6393ad94c37516ce5292f5704f882e51a924525
Daliah Aljutayli <DaliahAljutayli@gmail.com>
2019-08-25 19:46:31 +0300

    Python Casting
    Python Casting to String , int , float

---
+++ ‏‏‏‏‏‏Day 6 - Casting/Casting.py
@@ -0,0 +1,19 @@
+#Casting
+
+#1 Casting to int 
+x = int(1)
+y = int(2.8)
+z = int ("3")
+print('Casting int to int=',x,',Casting float to int=',y,' ,Casting string to int=',z)
+
+#2 Casting to float 
+x = float(1)
+y = float(2.8)
+z = float("3")
+print('Casting int to float=',x,',Casting float to float=',y,' ,Casting str to float=',z)
+
+#3 Casting to String 
+x = str(1)
+y = str(2.8)
+z = str("3")
+print('Casting int to str=',x,',Casting float to str=',y,' ,Casting str to str=',z)
