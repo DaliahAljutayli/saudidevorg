a1a13f29ce3a8fb417dca3c27ef1bbf0da137a30
Daliah Aljutayli <DaliahAljutayli@gmail.com>
2019-08-26 20:32:47 +0300

    String
    #Python Strings
    Single | Double Quotation
    Assign String to a Variable
    Multiline Strings
    Strings are Arrays
    Substring

---
+++ ‏‏‏‏‏‏‏‏Day 7 - Strings/String.py
@@ -0,0 +1,25 @@
+#Python Strings
+
+#Single | Double Quotation
+print('1-Single Quotation')
+print('1-Double Quotation')
+
+#Assign String to a Variable
+a = "2-Hello from a variable"
+print(a)
+
+#Multiline Strings
+b = """3-We are a lovely group of Strings♥
+       You may join Us..."""
+print(b)
+
+#Strings are Arrays
+c = "4-Hi with ♥"
+print(c ,"...Character at position 9 is: ",c[10])
+
+#Substring
+#Character 5 and 11 NOT INCLUDED
+c = "5-Hi with ♥"
+print(c ,"...Characters from position 5 to 11 is: ",c[5:11])
+ 
+
