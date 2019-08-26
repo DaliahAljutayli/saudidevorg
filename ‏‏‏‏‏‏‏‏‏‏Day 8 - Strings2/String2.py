b99ad4f90d0a7dd72cb14bba8fa4cdeb164ce7fb
Daliah Aljutayli <DaliahAljutayli@gmail.com>
2019-08-26 21:47:20 +0300

    String Part 2
    Removes whitespace from the beginning or end.
    The length of a string
    The string in lower case
    The string in Upper case
    Replaces a string
    Splits the string into substrings

---
+++ ‏‏‏‏‏‏‏‏‏‏Day 8 - Strings2/String2.py
@@ -0,0 +1,25 @@
+#String2
+
+#Removes whitespace from the beginning or the end.
+a = " Using strips(),Removes whitespace of beginning/end "
+print( a.strip() )
+
+#The length of a string
+b = "Using len() ,The length of this string is ⟵ "
+print( b , len(b) )
+
+#The string in lower case
+c = "Using lower() ,The String in Lower Case "
+print(c.lower() )
+
+#The string in Upper case
+d = "Using upper() ,The String in Upper Case "
+print(d.upper() )
+
+#Replaces a string
+e = "Using replace() ,Replaces a string"
+print(e.replace("Using replace() ,Replaces a string","⭐") )
+
+#Splits the string into substrings 
+f = "Splits the string into substrings using split()"
+print(f.split(",") )
