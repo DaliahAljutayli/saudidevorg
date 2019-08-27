475616fd195f1002c8d9915adc442be11d77d249
Daliah Aljutayli <DaliahAljutayli@gmail.com>
2019-08-27 22:13:55 +0300

    Python Operators
    •Arithmetic operators.
    •Assignment operators
    .•Comparison operators

---
+++ ‏‏‏‏‏‏‏‏‏‏‏‏‏‏Day 10 - Operators/Operators.py
@@ -0,0 +1,37 @@
+#Operators
+
+#Arithmetic Operators
+x = 3 
+y = 8
+add =x+y
+sub =x-y
+mult =x*y
+div =x/y
+Modulus =x%y
+Exponentitaion =x**y
+floorDiv =x//y
+
+print("Arithmetic: "," Add= ",add," Sub= ",sub,
+      " Mult= ",mult," Div= ",div,
+      " Modulus= ",Modulus," Exp= ",
+      Exponentitaion," Floor Div= ",floorDiv)
+
+#Assignment Operators
+x = 10
+x/=2
+print("Assignment: x+3: ",x)
+
+# Comparison Operators
+x = 3 
+y = 8
+
+print("Comparison: "," Equal: ",(x==y),", Not Equal: ",(x!=y),
+      " ,less than: ",(x<y)," ,Greater than or equal: ",(x>=y) )
+
+
+
+
+
+
+
+
