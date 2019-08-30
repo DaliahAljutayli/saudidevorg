f8d3e4aa0b8844877af4a284e95dfeefcfd36a42
Daliah Aljutayli <DaliahAljutayli@gmail.com>
2019-08-30 13:17:13 +0300

    Print String After Replacing and Changing
    text='Please, I want {} sandwishes and {} donutes'
    1- replace ( I ) with ( we )
    2- Change ( a ) with ( A )
    3- put 5 and 7 inside { }

---
+++ ‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏Day 12 - Practice/12.py
@@ -0,0 +1,6 @@
+num1 = 5
+num2 = 7
+text ='Please, I want {} sandwishes and {} donutes'
+
+print('The Original String : ',text )
+print('The Update of String: ',text.replace("I","We").replace("a","A").format(num1,num2))
