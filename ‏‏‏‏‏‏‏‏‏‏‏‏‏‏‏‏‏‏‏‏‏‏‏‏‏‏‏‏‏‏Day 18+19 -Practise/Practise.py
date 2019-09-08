84e0e0846a5c685072c671ee1f2ec62dd1e06f79
Daliah Aljutayli <DaliahAljutayli@gmail.com>
2019-09-08 19:11:24 +0300

    Add files via upload
     Q1: Write 4 List Methods.
     Q2: WriPrint this and Check- if 'python' in> tuple = ("java","python","swift")

---
+++ ‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏Day 18+19 -Practise/Practise.py
@@ -0,0 +1,25 @@
+#Practise : List + Tuples
+
+# Q1: Write 4 List Methods.
+print('Q1: Write 4 List Methods......')
+
+MyList = ['Daliah','Redna','Wafa','Amjad','Atheer','Eman']
+print('Computer Traines Name List: ',MyList)
+
+MyList.sort()
+print('Sorting Traines Name List : ' ,MyList)
+
+MyList.remove('Atheer')
+print('remove Name List : ' ,MyList)
+
+MyList.clear()
+print('Clearing Traines List : ',MyList)
+
+
+
+# Q2: WriPrint this and Check- if 'python' in> tuple = ("java","python","swift")
+print('\nQ2: WriPrint this and Check- if python in> tuple = ("java","python","swift")...')
+
+Tuple = ("java","python","swift") # Tuple not tuple
+if 'python' in Tuple: 
+    print('YES,Python inside in Tuple')
