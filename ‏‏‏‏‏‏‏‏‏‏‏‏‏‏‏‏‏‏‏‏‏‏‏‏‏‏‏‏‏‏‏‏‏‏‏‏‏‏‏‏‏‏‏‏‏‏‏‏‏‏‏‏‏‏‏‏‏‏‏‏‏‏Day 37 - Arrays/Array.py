34c1e418f39d1b98ea428c1ba66557eae858ae26
Daliah Aljutayli <DaliahAljutayli@gmail.com>
2019-10-18 17:14:22 +0300

    Array

---
+++ ‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏Day 37 - Arrays/Array.py
@@ -0,0 +1,12 @@
+#Array
+
+#store multiple values in one single variable
+Myarray = ["Daliah","Redna","Wafa","Amjad"]
+
+print("My Array:", Myarray)
+
+#referring to the indexnumber.
+print("Index one:",Myarray[1] )
+
+#the len method
+print("Length: ",len(Myarray))
