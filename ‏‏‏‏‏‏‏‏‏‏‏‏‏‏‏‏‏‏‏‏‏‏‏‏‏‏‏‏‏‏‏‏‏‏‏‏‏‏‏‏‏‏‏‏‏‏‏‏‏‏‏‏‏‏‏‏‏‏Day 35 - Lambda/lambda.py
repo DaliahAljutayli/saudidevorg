641e75a13f8f9622f77c9df71a045ec43d052418
Daliah Aljutayli <DaliahAljutayli@gmail.com>
2019-10-18 17:13:07 +0300

    Lambda

---
+++ ‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏Day 35 - Lambda/lambda.py
@@ -0,0 +1,9 @@
+# lambda 
+x = lambda a: a+ 10
+print ("1  argument: ", x(3) )
+
+x = lambda a,b: a+b
+print ("2 arguments: ", x(4,9) )
+
+x = lambda a,b,c: a+b+c
+print ("3 arguments: ", x(4,9,2) )
