4bd08d198c07c7d9e64033ac50db8b1ada5e7cca
Daliah Aljutayli <DaliahAljutayli@gmail.com>
2019-10-25 21:30:22 +0300

    ‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏Practise pip format()

---
+++ ‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏Day 67 + 68 - Practise/Practise.py
@@ -0,0 +1,18 @@
+# Pracise :
+
+# Q1: Input
+print("Please , Enter first letter of your name:")
+f_l = input()
+print("Please , Enter last letter of your name:")
+l_l=input()
+print("First letter of your name is ",f_l ,"\nlast letter is ",l_l)
+
+
+# Q2: Using of format()
+f_name = "Ahmad"
+l_name = "Ali"
+Balance = 53.44
+
+txt = "Dear {}{} , Your current balance is {} RS"
+print(txt.format(f_name,l_name,Balance))
+
