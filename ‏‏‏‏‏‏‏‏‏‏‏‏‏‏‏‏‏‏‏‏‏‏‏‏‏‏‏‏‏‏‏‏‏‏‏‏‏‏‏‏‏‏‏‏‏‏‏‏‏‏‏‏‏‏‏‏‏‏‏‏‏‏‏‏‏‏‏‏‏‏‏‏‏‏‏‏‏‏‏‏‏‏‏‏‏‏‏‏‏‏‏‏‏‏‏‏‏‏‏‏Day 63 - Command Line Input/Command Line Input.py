858835b1f891e6dec1ad45939d6bcbd05652b3a8
Daliah Aljutayli <DaliahAljutayli@gmail.com>
2019-10-25 21:28:23 +0300

    Command Line Input()

---
+++ ‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏‏Day 63 - Command Line Input/Command Line Input.py
@@ -0,0 +1,8 @@
+# Command Line Input
+
+print("Enter your name:")
+name = input()
+print("Enter your age:")
+age = input()
+print("The name: ",name ,"\nThe age :",age)
+#-------------------------------
